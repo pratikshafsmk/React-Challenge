8f4daba06fb75f3fe178175427b076db38385f9f
Naveen Pratiksha <npratiksha81@gmail.com>
2026-03-03 20:51:02 +0530

    Used setInterval to run increment in every second and used useRef so store the count value so that it doesnt eset on re-render wch will be happening in every second because of timer, and in handleStop usedClearInterval on ref.current to stop/lear Interval

---
+++ day-05-counter-with-timer/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useRef } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const ref = useRef(null);
+  const style = { border: "1px solid black" };
+  const handeStart = () => {
+    console.log("Inside Start handler");
+    ref.current = setInterval(increment, 500);
+    function increment() {
+      console.log("inside increment");
+      setCount((prev) => prev + 1);
+      //console.log(count);
+    }
+  };
+  const handleStop = () => {
+    clearInterval(ref.current);
+    ref.current = null;
+    console.log("Inside Stop");
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Count: {count}</h1>
+        <div style={{ display: "flex", gap: "15px" }}>
+          <button onClick={handeStart} style={style}>
+            Start
+          </button>
+          <button onClick={handleStop} style={style}>
+            Stop
+          </button>
+          <button onClick={() => setCount(0)} style={style}>
+            Reset
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
